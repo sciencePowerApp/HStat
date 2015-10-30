167d2763c38bdd7f27e8ab3a43aa56d4c901bc9a
andytwoods <andy.woods@xperiment.mobi>
2015-10-30 14:12:18 +0000

    added ian

--- install.bat
+++ install.bat
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+del bin\hx-mathml.zip
+7za a bin\hx-mathml.zip haxelib.json include.xml assets learnmath
+haxelib local bin\hx-mathml.zip
+=======
 del bin\HStat.zip
 7za a bin\HStat.zip haxelib.json hstat
 haxelib local bin\HStat.zip
+>>>>>>> 4b5ed6026acd8929824c3d2b3113e9cc0953be86
